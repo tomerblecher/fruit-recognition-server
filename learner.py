334df2dd51a325f9f29b43d20edcfe520a6acb9a
Tomer Blecher <tomer.blecher.brolix@gmail.com>
2020-05-02 14:21:57 +0300

    Server supports Nutrition data - Added some documentation

--- learner.py
+++ learner.py
@@ -4,20 +4,28 @@ from fastai import *
 from fastai.utils.show_install import *
 from fastai.widgets import *
 
+import time
 
 class Learner:
     def __init__(self, path , classes):
         self.model_classes = classes
         self.learner = load_learner(path)
 
-    def predict(self, img_bytes):
+    def predict(self, img_bytes, class_count):
+        """
+        Use the "FastAI"" API to predict the input image.
+        The function will get the precision percent & label data for the X most accurate classes.
+
+        :param byte[] img_bytes: Bytes of the input image
+        :param int class_count: How many prediction classes should be returned
+        :return: dictionary containing precision percent & label data ordered by accurate(First = highest)
+        """
         img = open_image(BytesIO(img_bytes))
-        # img = open_image((img_bytes))
         _,_, preds = self.learner.predict(img)
         # Get all best predictions
         preds_sorted, idxs = preds.sort(descending=True)
         res = []
-        for i in range(0,2):
+        for i in range(0,class_count):
             res.append({
                 "sPercent": int(np.round(100 * preds_sorted[i].item(), 2)),
                 "label":
--- server.py
+++ server.py
@@ -14,45 +14,72 @@ import time
 from PIL import Image
 
 app = Starlette(debug=True)
-# app.mount('/static', StaticFiles(directory='statics'), name='static')
+PATH = {
+    'tokens': 'data/tokens.json',
+    'labels': 'data/lable_json.json',
+    'nutrition_db': 'data/nutrition_db.json'
+}
 
 # Path for the images.
 img_folder = "data/imgs"
 
-print(__name__)
 tokens = []
-
-
-def load_tokens():
-    f = open('data/tokens.json', 'r')
-    data = json.load(f)
-    f.close()
-    return data
+nutrition = []
 
 
 def is_token_valid(request):
+    """
+        Validates the existence of a token from the client request with saved tokens list.
+
+        :param Request request : Request Incoming request from client.
+        :return bool: Token existence will return true, false otherwise
+    """
+
     return 'token' in request.headers and request.headers['token'] in tokens
 
 
-def get_model_classes():
-    f = open('data/lable_json.json', 'r')
+def load_json(path):
+    """
+        Loads a Json file and returns the deserialized object of that json.
+
+        :param string path: requested file path
+        :return dictionary json:  Deserialized representation of the json file
+    """
+
+    f = open(path, 'r')
     json_file = json.load(f)
     f.close()
     return json_file
 
 
-learn = Learner('data', get_model_classes())
+learn = Learner('data', load_json(PATH['labels']))
 
 
 @app.route("/classify", methods=["POST"])
 async def upload(request):
+    """
+        Predicts the classes for a given image.
+        The function will emit the predict method on the learner object and return the result.
+        Nutrition data will be added if the user flagged the 'extended' parameter as true.
+
+        :param Request request : Request Incoming request from client.
+        :return Response|JSONResponse: In case of an error in validation, an error response will be returned.
+                                       In case of a success,
+                                       a json response will be returned with the predicted classes & nutrition facts.
+    """
     if not is_token_valid(request):
         return Response(status_code=404)
     data = await request.form()
     print(request.headers)
-    bytes = await (data["file"].read())
+
     print(data['file'].filename)
-    response = learn.predict(bytes)
+    imgBytes = await (data['file'].read())
+    print("Image size: {}".format(len(imgBytes)))
+    response = learn.predict(imgBytes, 2)
+    if 'extended' in data and data['extended'].lower() == "true":
+        print("Need extra data")
+        for item in response:
+            item['extra'] = [d for d in nutrition if d['id'] == item['label']['nutrition_id']]
     res = {
         "timestamp": int(time.time()),
         "results": response
@@ -63,18 +90,38 @@ async def upload(request):
 
 @app.route('/class/fetch/all')
 async def fetch_all_labels(request):
+    """
+        Fetch label list from the configured Json file.
+
+        :param Request request : Request Incoming request from client.
+
+        :return Response|JSONResponse: In case of an error in validation, an error response will be returned.
+                                       In case of a success, a json response will be returned with the labels list.
+    """
+
     if not is_token_valid(request):
         return Response(status_code=404)
-    return JSONResponse(get_model_classes())
+    return JSONResponse(load_json(PATH['labels']))
 
 
 @app.route('/')
 async def default_route(request):
+    """
+        Since we are exposing the server to the world, lets try to hide it from unwanted visitors.
+    """
     return Response(status_code=404)
 
 
 @app.route('/learn/add', methods=["POST"])
 async def save_img(request):
+    """
+        Save a new incoming image on the server.
+        Request will go through validation before saving.
+
+        :param Request request : Request Incoming request from client.
+        :return Response|JSONResponse: In case of an error in validation, an error response will be returned.
+                                       In case of a success, a json response will be returned with status 200.
+    """
     if not is_token_valid(request):
         return Response(status_code=404)
     data = await request.form()
@@ -98,7 +145,8 @@ async def save_img(request):
 
 
 if __name__ == "__main__":
-    tokens = load_tokens()
+    tokens = load_json(PATH['tokens'])
+    nutrition = load_json(PATH['nutrition_db'])
     print(tokens)
     if len(sys.argv) > 1:
         if sys.argv[1] == "live":
